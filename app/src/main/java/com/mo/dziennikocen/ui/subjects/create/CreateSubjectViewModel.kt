a1a9e7f52a20e95d79c2198026748d85a13ce43d
micos <micos@interia.pl>
2021-12-29 17:02:52 +0100

    add data

--- app/src/main/java/com/mo/dziennikocen/ui/subjects/create/CreateSubjectViewModel.kt
+++ app/src/main/java/com/mo/dziennikocen/ui/subjects/create/CreateSubjectViewModel.kt
@@ -1,5 +1,6 @@
 package com.mo.dziennikocen.ui.subjects.create
 
+import android.view.View
 import androidx.lifecycle.*
 import com.mo.dziennikocen.domain.usecases.validation.subject.ValidateSubjectNameUseCase
 import com.mo.dziennikocen.extensions.result
@@ -9,12 +10,24 @@ class CreateSubjectViewModel(
 ) : ViewModel() {
 
     val subjectName = MutableLiveData("")
+    val subjectNameVisibility = MutableLiveData(View.VISIBLE)
     val areInputsCorrect = MediatorLiveData<Boolean>().apply {
         addSource(subjectName) {
             this.value = isSubjectNameCorrect()
+            changeVisibilityOfSubjectInfo()
         }
     }
 
     private fun isSubjectNameCorrect() =
         validateSubjectNameUseCase.invoke(subjectName.value ?: "").result()
+
+    private fun changeVisibilityOfSubjectInfo() {
+        areInputsCorrect.value?.let {
+            if (it) {
+                subjectNameVisibility.value = View.INVISIBLE
+            } else {
+                subjectNameVisibility.value = View.VISIBLE
+            }
+        }
+    }
 }
